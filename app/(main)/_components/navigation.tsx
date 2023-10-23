4ded13df54f62e1bdb980a09f39109b3cf77d824
madhurpant <madhurp321@gmail.com>
2023-10-23 18:30:54 +0530

    Trash Box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 
@@ -12,6 +12,8 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { TrashBox } from "./trashbox";
 
 export const Navigation = () => {
 
@@ -128,6 +130,16 @@ export const Navigation = () => {
                 </div>
                 <div className="mt-4">
                   <DocumentList />
+                  <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+
+                  <Popover>
+                    <PopoverTrigger className="w-full mt-4">
+                      <Item label="Trash" icon={Trash} />
+                    </PopoverTrigger>
+                    <PopoverContent className="p-0 w-72" side={isMobile ? "bottom" : "right"}>
+                      <TrashBox />
+                    </PopoverContent>
+                  </Popover>
                 </div>
 
                 <div 
