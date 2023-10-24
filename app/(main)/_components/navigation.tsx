1bba5408c2f4012b8210d7185d22ef76a5f694d4
madhurpant <madhurp321@gmail.com>
2023-10-24 22:36:13 +0530

    Search

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -14,9 +14,10 @@ import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { TrashBox } from "./trashbox";
+import { useSearch } from "@/hooks/use-search";
 
 export const Navigation = () => {
-
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -124,7 +125,7 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
-                    <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+                    <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
                     <Item label="Settings" icon={Settings} onClick={() => {}} />
                     <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
                 </div>
